5bdcdce593971f531691d1321b2998368594a5eb
botcitrus <85682253+botcitrus@users.noreply.github.com>
2023-01-04 21:10:42 +0300

    Update modals.py

--- cogs/modals.py
+++ cogs/modals.py
@@ -1,33 +1,54 @@
 import discord
 from discord.ext import commands
 
-class SurveyModal(discord.ui.Modal, title = "Заявка на пост администратора!"):
+class StaffModal(discord.ui.Modal, title = "Заявка на пост стаффа!"):
     name = discord.ui.TextInput(label = "Имя?")
     age = discord.ui.TextInput(label = "Возвраст?")
     sentry = discord.ui.TextInput(label = "Часовой пояс от мск?")
     whyou = discord.ui.TextInput(label = "Почему именно вы?", style = discord.TextStyle.paragraph)
     yourslf = discord.ui.TextInput(label = "Немного о себе", style = discord.TextStyle.paragraph)
     async def on_submit(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"Имя: {self.name}\nВозвраст: {self.age}\nЧасовой поис: {self.sentry}\nПочему именно вы: {self.whyou}\nНемного о себе")
+        await interaction.user.send(f"Имя: {self.name}\nВозвраст: {self.age}\nЧасовой поис: {self.sentry}\nПочему именно вы: {self.whyou}\nНемного о себе")
+        
+class AdmModal(discord.ui.Modal, title = "Заявка на пост администрации!"):
+    name = discord.ui.TextInput(label = "Имя?")
+    age = discord.ui.TextInput(label = "Возвраст?")
+    sentry = discord.ui.TextInput(label = "Часовой пояс от мск?")
+    whyou = discord.ui.TextInput(label = "Почему именно вы?", style = discord.TextStyle.paragraph)
+    yourslf = discord.ui.TextInput(label = "Немного о себе", style = discord.TextStyle.paragraph)
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.user.send(f"Имя: {self.name}\nВозвраст: {self.age}\nЧасовой поис: {self.sentry}\nПочему именно вы: {self.whyou}\nНемного о себе")
+        
+class EveModal(discord.ui.Modal, title = "Заявка на пост ивентера!"):
+    name = discord.ui.TextInput(label = "Имя?")
+    age = discord.ui.TextInput(label = "Возвраст?")
+    sentry = discord.ui.TextInput(label = "Часовой пояс от мск?")
+    whyou = discord.ui.TextInput(label = "Почему именно вы?", style = discord.TextStyle.paragraph)
+    yourslf = discord.ui.TextInput(label = "Немного о себе", style = discord.TextStyle.paragraph)
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.user.send(f"Имя: {self.name}\nВозвраст: {self.age}\nЧасовой поис: {self.sentry}\nПочему именно вы: {self.whyou}\nНемного о себе")
         
 class Select(discord.ui.Select):
     def __init__(self):
         options=[
-            discord.SelectOption(label="Red", emoji="❤️", description="This is option 1!"),
-            discord.SelectOption(label="Blue", emoji="💙", description="This is option 2!"),
-            discord.SelectOption(label="Green", emoji="💚", description="This is option 3!")
+            discord.SelectOption(label="Staff", emoji="🔧", description="Заявка на роль стаффа"),
+            discord.SelectOption(label="Administrator", emoji="⚒", description="Заявка на роль администрации"),
+            discord.SelectOption(label="Eventer", emoji="🔮", description="Заявка на роль ивентера")
             ]
-        super().__init__(placeholder="Select an option", max_values=1, min_values=1, options=options)
+        super().__init__(placeholder="Выберите нужный пункт:", max_values = 1, min_values = 1, options = options)
     async def callback(self, interaction: discord.Interaction):
-        if self.values[0] == "Red":
-            await interaction.response.send_modal(SurveyModal())
-            await interaction.response.edit_message(content = "This is the first option from the entire list!")
-        elif self.values[0] == "Blue":
-            await interaction.response.send_modal(SurveyModal())
-            await interaction.response.send_message("This is the second option from the list entire wooo!", ephemeral=False)
-        elif self.values[0] == "Green":
-            await interaction.response.send_modal(SurveyModal())
-            await interaction.response.send_message("Third One!", ephemeral=False)
+        if self.values[0] == "Staff":
+            await interaction.response.send_modal(StaffModal())
+            embed = discord.Embed(title = "STAFF", description = "Заявка на роль staff принята!")
+            await interaction.response.send_message(embed = embed)
+        elif self.values[0] == "Administrator":
+            await interaction.response.send_modal(AdmModal())
+            embed = discord.Embed(title = "ADMINISTRATOR", description = "Заявка на роль administrator принята!")
+            await interaction.response.send_message(embed = embed)
+        elif self.values[0] == "Eventer":
+            await interaction.response.send_modal(EveModal())
+            embed = discord.Embed(title = "EVENTER", description = "Заявка на роль eventer принята!")
+            await interaction.response.send_message(embed = embed)
             
 class Buttons(discord.ui.View):
     def __init__(self, *, timeout = 180):
@@ -48,8 +69,9 @@ class Modal(commands.Cog):
         self.client = client
       
     @commands.command()
-    async def modal(self, ctx):
-        await ctx.send("заявка на адм", view = Buttons())
+    async def набор(self, ctx):
+        embed = discord.Embed(title = "Набор на Staff, Administrator, Eventer", description = "Выберите нужную категорию и заполните заявку!")
+        await ctx.send(embed = embed, view = ModalView())
       
 async def setup(client):
     await client.add_cog(Modal(client))
