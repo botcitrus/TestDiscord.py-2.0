d902fd091d157c36a04c77e8f9bc27f89cfa39d3
botcitrus <85682253+botcitrus@users.noreply.github.com>
2023-01-04 09:27:26 +0300

    Update selectmenu.py

--- cogs/selectmenu.py
+++ cogs/selectmenu.py
@@ -4,13 +4,18 @@ from discord.ext import commands
 class Select(discord.ui.Select):
     def __init__(self):
         options=[
-            discord.SelectOption(label="Option 1",emoji="👌",description="This is option 1!"),
-            discord.SelectOption(label="Option 2",emoji="✨",description="This is option 2!"),
-            discord.SelectOption(label="Option 3",emoji="🎭",description="This is option 3!")
+            discord.SelectOption(label="Red",emoji=":red:",description="This is option 1!"),
+            discord.SelectOption(label="Blue",emoji=":blue:",description="This is option 2!"),
+            discord.SelectOption(label="Green",emoji=":green:",description="This is option 3!")
             ]
         super().__init__(placeholder="Select an option", max_values=1, min_values=1, options=options)
     async def callback(self, interaction: discord.Interaction):
-        await interaction.response.send_message(content=f"Your choice is {self.values[0]}!", ephemeral=True)
+        if self.values[0] == "Red":
+            await interaction.response.edit_message(content = "This is the first option from the entire list!")
+        elif self.values[0] == "Blue":
+            await interaction.response.send_message("This is the second option from the list entire wooo!", ephemeral=False)
+        elif self.values[0] == "Green":
+            await interaction.response.send_message("Third One!", ephemeral=False)
 
 class SelectView(discord.ui.View):
     def __init__(self, *, timeout = 180):
