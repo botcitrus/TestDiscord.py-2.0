b5ad2a03c34244832fc026ef4b273d97059ea232
botcitrus <85682253+botcitrus@users.noreply.github.com>
2023-01-09 20:40:57 +0300

    Update modals.py

--- cogs/modals.py
+++ cogs/modals.py
@@ -3,59 +3,32 @@ from discord.ext import commands
 from pymongo import MongoClient
 
 
-class Buttons(discord.ui.View):
-    def __init__(self, *, timeout = 180):
-        super().__init__(timeout=timeout)
-        
-    @discord.ui.button(label = "Принять", style = discord.ButtonStyle.green)
-    async def clicktru(self, interaction: discord.Interaction, child: discord.ui.Button):
-        for child in self.children:
-            child.disabled = True
-        guild = interaction.guild
-        channel = discord.utils.get(guild.text_channels, name = "приняты")
-        await channel.send(f"{interaction.user}, ваша заявка принята!")
-        await interaction.response.edit_message(view = self)
-        await interaction.response.send_message("Успешно", ephemeral = True)
-        
-    @discord.ui.button(label = "Отказать", style = discord.ButtonStyle.red)
-    async def clickfalse(self, interaction: discord.Interaction, child: discord.ui.Button):
-        for child in self.children:
-            child.disabled = True
-        user = interaction.user
-        await user.send(f"{user}, ваша заявка отклонена!")
-        await interaction.response.edit_message(view = self)
-        await interaction.response.send_message("Успешно", ephemeral = True)
-
 class RegModal(discord.ui.Modal, title = "Регистрация аккаунта на фейсите!"):
-    def __init__(self):
-        self.cluster = MongoClient("mongodb+srv://Setroom:CFLrxCSX0fzBIMlA@cluster0.l9fw9.mongodb.net/ecodb?retryWrites=true&w=majority")
-        self.colluser = self.cluster.faceit.user
-        self.game = self.cluster.faceit.game
-        
     name = discord.ui.TextInput(label = "Имя в игре:", min_length = 2, max_length = 15)
     age = discord.ui.TextInput(label = "id в игре:", min_length = 8, max_length = 20)
     whyou = discord.ui.TextInput(label = "Почему именно вы?", style = discord.TextStyle.short)
     async def on_submit(self, interaction: discord.Interaction):
+        cluster = MongoClient("mongodb+srv://Setroom:CFLrxCSX0fzBIMlA@cluster0.l9fw9.mongodb.net/ecodb?retryWrites=true&w=majority")
+        colluser = cluster.faceit.user
         guild = interaction.guild
         user = interaction.user
-        await interaction.response.send_message("Мы получили вашу заявку!", ephemeral = True)
+        await interaction.response.send_message("Вы зарегистрированны!", ephemeral = True)
         
-class Select(discord.ui.Select):
-    def __init__(self):
-        options=[
-            discord.SelectOption(label="Registration", emoji="📝", description="Регистрация аккаунта!"),
-            ]
-        super().__init__(placeholder="Выберите нужный пункт:", options = options) #max_values = 1, min_values = 1,
-    async def callback(self, interaction: discord.Interaction):
-        while True:
-            if self.values[0] == "Registration":
-                await interaction.response.send_modal(RegModal())
-                await interaction.response.send_message("Вы успешно зарегистрированы!", ephemeral = True)
-            
-class ModalView(discord.ui.View):
+class Buttons(discord.ui.View):
     def __init__(self, *, timeout = 180):
         super().__init__(timeout=timeout)
-        self.add_item(Select())
+        
+    @discord.ui.button(label = "Регистрация", style = discord.ButtonStyle.green)
+    async def click(self, interaction: discord.Interaction, child: discord.ui.Button):
+        guild = interaction.guild
+        user = interaction.user
+        await interaction.response.send_modal(RegModal())
+        await interaction.response.send_message("Зарегистрируйтесь!", ephemeral = True)
+            
+#class ModalView(discord.ui.View):
+    #def __init__(self, *, timeout = 180):
+        #super().__init__(timeout=timeout)
+        #self.add_item(())
         
 class Modal(commands.Cog):
     def __init__(self, client):
@@ -63,8 +36,8 @@ class Modal(commands.Cog):
       
     @commands.command()
     async def регистрация(self, ctx):
-        embed = discord.Embed(title = "Набор на Staff, Administrator, Eventer", description = "Выберите нужную категорию и заполните заявку!")
-        await ctx.send(embed = embed, view = ModalView())
+        embed = discord.Embed(title = "Регистрация аккаунта!", description = "Чтобы зарегистрироваться нужно нажать на кнопку ниже и заполнить анкету по примеру ниже!")
+        await ctx.send(embed = embed, view = Buttons())
       
 async def setup(client):
     await client.add_cog(Modal(client))
