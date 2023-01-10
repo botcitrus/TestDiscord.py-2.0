710ab52d2a18713a56fb607012df60510a961fdf
botcitrus <85682253+botcitrus@users.noreply.github.com>
2023-01-10 21:29:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,39 +28,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("--------")
-    await client.change_presence(activity=discord.Streaming(name=f'Cyber Faceit', url="https://www.twitch.tv/qrushcsgo")
-    
-@client.command()
-@commands.is_owner()
-async def eval(ctx, *, ucode=None):
-    tru = discord.utils.get(client.emojis, name='yes')
-    err = discord.utils.get(client.emojis, name='no')
-    code = '\n'.join(ucode.split('\n')[1:])[:-3] if ucode.startswith('```') and ucode.endswith('```') else ucode
-    libs = {
-        'discord': discord,
-        'commands': commands,
-        'bot': client,
-        'client': client,
-        'ctx': ctx
-    }
-    start = time.time()
-    try:
-        reval = await aeval.aeval(code, libs, {})
-        end = time.time() - start
-        emb = discord.Embed(title=f'{tru} Успешно:',
-            description=f'**Время выполнения** - {end}\n'
-                        f'**Входные данные** - \n```py\n{code}\n```\n'
-                        f'**Выходные данные** - \n```py\n{reval}\n```\n',
-            color=discord.Color.green())
-        await ctx.send(embed=emb) 
-    except Exception as exception:
-        end = time.time() - start
-        emb = discord.Embed(title=f'{err} Ошибка:',
-            description=f'**Время выполнения** - {end}\n'
-                        f'**Входные данные** - \n```py\n{code}\n```\n'
-                        f'**Выходные данные** - \n```py\n{exception}\n```\n',
-            color=discord.Color.red())
-        await ctx.send(embed=emb)
+    await client.change_presence(activity=discord.Streaming(name=f'Galaxy Faceit', url="https://www.twitch.tv/qrushcsgo")
     
 async def load():
     for file in os.listdir("./cogs"):
