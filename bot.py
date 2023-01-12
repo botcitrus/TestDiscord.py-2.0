87a8ad1c1ad2149822e422ea2131ff6290b04306
botcitrus <85682253+botcitrus@users.noreply.github.com>
2023-01-12 18:48:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,6 +36,6 @@ async def load():
 
 async def bot():
     await load()
-    await client.start('MTA1OTg4NjM3ODIyMzE2MTM4NA.GStMiN.FbPpxtCwK4PgOE423OPIZ8noyJwQnjKrTPyyC0')
+    await client.start('MTA2MzEyMTkxMTE4NzUyNTc2Mg.G01aPf.ke3u0OQxn293Cb5KTQ6-Tabpy9nwJO5CWA9jH0')
     
 asyncio.run(bot())
