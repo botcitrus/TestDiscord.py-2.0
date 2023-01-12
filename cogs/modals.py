7d35af25b58a4bf132f923aca5e6c647c685a5a0
botcitrus <85682253+botcitrus@users.noreply.github.com>
2023-01-12 20:02:40 +0300

    Update modals.py

--- cogs/modals.py
+++ cogs/modals.py
@@ -9,8 +9,11 @@ class ModModal(discord.ui.Modal, title = "Заявка на модератора
     active = discord.ui.TextInput(label = "Активность:", placeholder = "5-6 часов в день")
     FA2 = discord.ui.TextInput(label = "2FA:", placeholder = "есть/нет")
     async def on_submit(self, interaction: discord.Interaction):
+        guild = interaction.guild
+        channel = discord.utils.get(guild.channels, id = "1060262034496815176")
         embed = discord.Embed(title = "Заявка на Moderator", description = f"Пользователь: {interaction.user}\nВозвраст: {self.age}\nВремя на сервере: {self.time}\nОпыт работы: {self.experions}\nАктивность: {self.active}\n2FA: {self.FA2}")
         await interaction.response.send_message("Заявка получена и будет рассмотрена в ближайщее время!", ephemeral = True)
+        await channel.send(embed = embed)
         
 class PMModal(discord.ui.Modal, title = "Заявка на пиар-менеджера:"):
     age = discord.ui.TextInput(label = "Возвраст:", min_length = 1, max_length = 2, placeholder = "14")
@@ -18,8 +21,10 @@ class PMModal(discord.ui.Modal, title = "Заявка на пиар-менедж
     experions = discord.ui.TextInput(label = "Опыт работы:", placeholder = "2 года, был на серверах: Saints, PTO, STO и т.д.", style=discord.TextStyle.short)
     active = discord.ui.TextInput(label = "Количество партнёрок:", placeholder = "20-30 в день", style=discord.TextStyle.short)
     async def on_submit(self, interaction: discord.Interaction):
+        guild = interaction.guild
+        channel = discord.utils.get(guild.channels, id = "1060262034496815176")
         embed = discord.Embed(title = "Заявка на Piar Manager", description = f"Пользователь: {interaction.user}\nВозвраст: {self.age}\nВремя на сервере: {self.time}\nОпыт работы: {self.experions}\nКоличество партнёрок: {self.active}")
-        await interaction.response.send_message("Заявка получена и будет рассмотрена в ближайщее время!", ephemeral = True)
+        await channel.send(embed = embed)
 
 class DesModal(discord.ui.Modal, title = "Заявка на дизайнера:"):
     age = discord.ui.TextInput(label = "Возвраст:", min_length = 1, max_length = 2, placeholder = "14")
@@ -28,8 +33,11 @@ class DesModal(discord.ui.Modal, title = "Заявка на дизайнера:"
     device = discord.ui.TextInput(label = "Устройство:", placeholder = "телефон/пк")
     rob = discord.ui.TextInput(label = "Готовы работать бесплатно, за пиар:", placeholder = "да/нет")
     async def on_submit(self, interaction: discord.Interaction):
+        guild = interaction.guild
+        channel = discord.utils.get(guild.channels, id = "1060262034496815176")
         embed = discord.Embed(title = "Заявка на Designer", description = f"Пользователь: {interaction.user}\nВозвраст: {self.age}\nВремя на сервере: {self.time}\nОпыт работы: {self.experions}\nДевайс: {self.device}\nГотовы работать бесплатно, за пиар: {self.rob}")
         await interaction.response.send_message("Заявка получена и будет рассмотрена в ближайщее время!", ephemeral = True)
+        await channel.send(embed = embed)
         
 class Select(discord.ui.Select):
     def __init__(self):
