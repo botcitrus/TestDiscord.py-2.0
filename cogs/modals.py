74ed15bb33cee4bb8fc7db03e1c8429e2cd8c851
botcitrus <85682253+botcitrus@users.noreply.github.com>
2023-01-12 19:23:37 +0300

    Update modals.py

--- cogs/modals.py
+++ cogs/modals.py
@@ -2,20 +2,60 @@ import discord
 from discord.ext import commands
 from pymongo import MongoClient
 
+class ModModal(discord.ui.Modal, title = "Заявка на модератора:"):
+    age = discord.ui.TextInput(label = "Возвраст:", min_length = 1, max_length = 2, placeholder = "14")
+    time = discord.ui.TextInput(label = "Время на сервере:", min_length = 8, max_length = 20, placeholder = "1-2 месяца")
+    experions = discord.ui.TextInput(label = "Опыт работы:", placeholder = "2 года, был на серверах: Saints, PTO, STO и т.д.", style=discord.TextStyle.short)
+    active = discord.ui.TextInput(label = "Активность:", placeholder = "5-6 часов в день", style=discord.TextStyle.short)
+    pravila = discord.ui.TextInput(label = "Знание правил:", placeholder = "да знаю", style=discord.TextStyle.short)
+    FA2 = discord.ui.TextInput(label = "2FA:", placeholder = "есть/нет", style=discord.TextStyle.short)
+    async def on_submit(self, interaction: discord.Interaction):
+        embed = discord.Embed(title = "Заявка на Moderator", description = f"Пользователь: {interaction.user}\nВозвраст: {self.age}\nВремя на сервере: {self.time}\nОпыт работы: {self.experions}\nАктивность: {self.active}\nЗнание правил: {self.pravila}\n2FA: {self.FA2}")
+        await interaction.response.send_message("Заявка получена и будет рассмотрена в ближайщее время!", ephemeral = True)
+        
+class PMModal(discord.ui.Modal, title = "Заявка на пиар-менеджера:"):
+    age = discord.ui.TextInput(label = "Возвраст:", min_length = 1, max_length = 2, placeholder = "14")
+    time = discord.ui.TextInput(label = "Время на сервере:", min_length = 8, max_length = 20, placeholder = "1-2 месяца")
+    experions = discord.ui.TextInput(label = "Опыт работы:", placeholder = "2 года, был на серверах: Saints, PTO, STO и т.д.", style=discord.TextStyle.short)
+    active = discord.ui.TextInput(label = "Количество партнёрок:", placeholder = "20-30 в день", style=discord.TextStyle.short)
+    async def on_submit(self, interaction: discord.Interaction):
+        embed = discord.Embed(title = "Заявка на Piar Manager", description = f"Пользователь: {interaction.user}\nВозвраст: {self.age}\nВремя на сервере: {self.time}\nОпыт работы: {self.experions}\nКоличество партнёрок: {self.active}")
+        await interaction.response.send_message("Заявка получена и будет рассмотрена в ближайщее время!", ephemeral = True)
+
+class DesModal(discord.ui.Modal, title = "Заявка на дизайнера:"):
+    age = discord.ui.TextInput(label = "Возвраст:", min_length = 1, max_length = 2, placeholder = "14")
+    time = discord.ui.TextInput(label = "Время на сервере:", min_length = 8, max_length = 20, placeholder = "1-2 месяца")
+    experions = discord.ui.TextInput(label = "Опыт работы:", placeholder = "2 года, был на серверах: Saints, PTO, STO и т.д.", style=discord.TextStyle.short)
+    device = discord.ui.TextInput(label = "Устройство:", placeholder = "телефон/пк", style=discord.TextStyle.short)
+    art = discord.ui.TextInput(label = "Примеры работ:", placeholder = "в лс гл.Организатор", style=discord.TextStyle.short)
+    rob = discord.ui.TextInput(label = "Готовы работать бесплатно, за пиар:", placeholder = "да/нет", style=discord.TextStyle.short)
+    async def on_submit(self, interaction: discord.Interaction):
+        embed = discord.Embed(title = "Заявка на Designer", description = f"Пользователь: {interaction.user}\nВозвраст: {self.age}\nВремя на сервере: {self.time}\nОпыт работы: {self.experions}\nДевайс: {self.device}\nПримеры работ: {self.art}\nГотовы работать бесплатно, за пиар: {self.rob}")
+        await interaction.response.send_message("Заявка получена и будет рассмотрена в ближайщее время!", ephemeral = True)
+        
 class Select(discord.ui.Select):
     def __init__(self):
-        options=[
-            discord.SelectOption(label = "Registration", emoji = "👌", description = "This is option 1!")
+        options = [
+            discord.SelectOption(label = "Moderator", emoji = "⚒", description = "Заявка на модератора!"),
+            discord.SelectOption(label = "Request-PM", emoji = "🔎", description = "Заявка на пиар-менеджера!"),
+            discord.SelectOption(label = "Designer", emoji = "🖌", description = "Заявка на дизайнера!")
             ]
         super().__init__(placeholder = "Выберите нужное:", options = options)
     async def callback(self, interaction: discord.Interaction):
         while True:
-            if self.values[0] == "Registration":
-                await interaction.response.edit_message(content="This is the first option from the entire list!")
+            if self.values[0] == "Moderator":
+                await interaction.response.send_modal(ModModal())
+                await interaction.response.edit_message(content="Заполните заявку!")
+            elif self.values[0] == "Request-PM":
+                await interaction.response.send_modal(PMModal())
+                await interaction.response.edit_message(content="Заполните заявку!")
+            elif self.values[0] == "Designer":
+                await interaction.response.send_modal(DesModal())
+                await interaction.response.edit_message(content="Заполните заявку!")
 
 class SelectView(discord.ui.View):
     def __init__(self, *, timeout = None):
-        super().__init__(timeout=timeout)
+        super().__init__(timeout = timeout)
         self.add_item(Select())
 
 class RegModal(discord.ui.Modal, title = "Регистрация аккаунта:"):
@@ -48,7 +88,7 @@ class Modal(commands.Cog):
     @commands.command()
     async def регистрация(self, ctx):
         embed = discord.Embed(title = "Регистрация аккаунта!", description = "Чтобы зарегистрироваться нужно нажать на кнопку ниже и заполнить анкету по примеру ниже!")
-        await ctx.send(embed = embed, view = Buttons())
+        await ctx.send(embed = embed, view = SelectView())
       
 async def setup(client):
     await client.add_cog(Modal(client))
