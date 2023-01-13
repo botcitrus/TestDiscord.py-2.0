bfbe08b501d101446c25332864664ca3228004d5
botcitrus <85682253+botcitrus@users.noreply.github.com>
2023-01-13 20:32:44 +0300

    Update faceit.py

--- cogs/faceit.py
+++ cogs/faceit.py
@@ -26,11 +26,53 @@ null_color=discord.Color.from_rgb(47,49,56)
 class Faceit(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.cluster = MongoClient("mongodb+srv://Setroom:CFLrxCSX0fzBIMlA@cluster0.l9fw9.mongodb.net/ecodb?retryWrites=true&w=majority")
+        self.colluser = self.cluster.faceit.user
+        self.collgame = self.cluster.faceit.game
 	
     @commands.Cog.listener()
     async def on_ready(self):
         print("Faceit cog loaded.")
-
+	
+    @commands.command()
+    async def game(self, ctx, name1: str = None, tag1: str = None, name2: str = None, tag2: str = None):
+        tru = discord.utils.get(self.client.emojis, name='yes')
+        err = discord.utils.get(self.client.emojis, name='no')
+        if name1 is None:
+            embed = discord.Embed(
+                title = f"{err} Ошибка:",
+                description = f"Укажите название первой команды!",
+                color = discord.Color.red()
+	    )
+            await ctx.send(embed = embed)
+        elif tag1 is None:
+            embed = discord.Embed(
+                title = f"{err} Ошибка:",
+                description = f"Укажите тег первой команды!",
+                color = discord.Color.red()
+	    )
+            await ctx.send(embed = embed)
+        elif name2 is None:
+            embed = discord.Embed(
+                title = f"{err} Ошибка:",
+                description = f"Укажите название второй команды!",
+                color = discord.Color.red()
+	    )
+            await ctx.send(embed = embed)
+        elif tag2 is None:
+            embed = discord.Embed(
+                title = f"{err} Ошибка:",
+                description = f"Укажите тег второй команды!",
+                color = discord.Color.red()
+	    )
+            await ctx.send(embed = embed)
+        else:
+            embed = discord.Embed(
+                title = f"{tru} Успешно:",
+                description = f"Игра команд: {name1} vs {name2} успешно создана!\nБот готов принимать ставки",
+                color = 0x00FFFF
+	    )
+            await ctx.send(embed = embed)
         
 async def setup(client):
     await client.add_cog(Faceit(client))
